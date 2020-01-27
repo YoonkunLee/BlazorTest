0790a4058ffb143309bbc1c9c4c2eea9dcf18ee9
YoonkunLee <ydx9474@aut.ac.nz>
2020-01-27 19:58:57 +1300

    Update Razor Component

---
+++ MyPoject/Pages/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyPoject.Pages
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Please Enter Your UserID")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Please Enter Your Password")]
+        public string UserPassword { get; set; }
+    }
+}
