31c93532d286e29003d30377631de46d9285a713
YoonkunLee <ydx9474@aut.ac.nz>
2020-01-28 21:28:19 +1300

    Create Customer Page. Also done "cruD"

---
+++ MyPoject/Pages/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPoject.Pages
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string AccountDefaultUrl { get; set; }
+    }
+}
